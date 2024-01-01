cf5d344ffe316cbfaae42e08cc25d7a131fed7b0
Steve Curtis <steve.curtis@truthmd.com>
2024-01-01 16:28:17 -0500

    add category service test

--- ime-server/src/main/java/com/scurtis/ime/Application.java
+++ ime-server/src/main/java/com/scurtis/ime/Application.java
@@ -1,12 +1,9 @@
 package com.scurtis.ime;
 
-import com.scurtis.ime.repository.CategoryRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
-//@EnableR2dbcRepositories(basePackageClasses = {CategoryRepository.class})
 public class Application {
 
     public static void main(String[] args) {
---
+++ ime-server/src/test/java/com/scurtis/ime/service/CategoryServiceTest.java
@@ -0,0 +1,64 @@
+package com.scurtis.ime.service;
+
+import com.scurtis.ime.entity.Category;
+import com.scurtis.ime.repository.CategoryRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class CategoryServiceTest {
+
+    private CategoryService categoryService;
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        categoryService = spy(new CategoryService(categoryRepository));
+    }
+
+    @AfterEach
+    void afterEach() {
+        verifyNoMoreInteractions(categoryRepository);
+        verifyNoMoreInteractions(categoryService);
+    }
+
+    @Test
+    void saveCategorySuccess() {
+        Category category = getCategory();
+
+        when(categoryRepository.save(category)).thenReturn(Mono.just(category));
+
+        Mono<Category> result = categoryService.saveCategory(category);
+
+        StepVerifier.create(result)
+            .thenConsumeWhile(r -> {
+                assertEquals("name", r.getName());
+                return true;
+            })
+            .verifyComplete();
+
+        verify(categoryRepository).save(category);
+        verify(categoryService).saveCategory(category);
+    }
+
+    private Category getCategory() {
+        Category category = new Category();
+        category.setName("name");
+        return category;
+    }
+
+}
